50bf2f803e1e2f93e9e2f21063fb7367ac671d1c
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-10-25 16:12:22 -0400

    just got the framework for compact and expanded VCs. Code files added, logic for transition based on button push added, but nothing in Storyboard yet

---
+++ MessagesExtension/ExpandedVC.swift
@@ -0,0 +1,36 @@
+//
+//  ExpandedVC.swift
+//  MatchUp
+//
+//  Created by Drew Lanning on 10/25/16.
+//  Copyright © 2016 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+import Messages
+
+class ExpandedVC: MSMessagesAppViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
