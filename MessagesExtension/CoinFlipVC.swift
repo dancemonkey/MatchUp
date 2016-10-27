d05750019cfd7a45668881b5c303c845c9d17bf7
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-10-27 11:14:55 -0400

    message placed in transcript, need to change bg color and handle opening message on far end

--- MessagesExtension/CoinFlipVC.swift
+++ MessagesExtension/CoinFlipVC.swift
@@ -22,43 +22,38 @@ class CoinFlipVC: MSMessagesAppViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    let tap = UITapGestureRecognizer(target: self, action: #selector(CoinFlipVC.coinTapped(sender:)))
+    let tap = UITapGestureRecognizer(target: self, action: #selector(CoinFlipVC.composeMessage))
     coinImg.addGestureRecognizer(tap)
     
     gameState = nextGameState(from: gameState)
   }
   
   @IBAction func cancelPressed(sender: UIButton) {
-    Utils.animateButton(sender, withTiming: 0.05) { 
+    Utils.animateButton(sender, withTiming: 0.05) {
       self.dismiss(animated: true, completion: nil)
     }
   }
   
-  func coinTapped(sender: UIImageView) {
-    print("coin tapped")
-    if let convo = activeConversation {
+  func composeMessage() {
+    let convo = activeConversation ?? MSConversation()
     
-      print("doing the shit")
-      
-      gameState = nextGameState(from: gameState)
-      
-      let session = convo.selectedMessage?.session ?? MSSession()
-      
-      let layout = MSMessageTemplateLayout()
-      layout.imageTitle = "COIN FLIP CHALLENGE!"
-      layout.image = sender.image
-      layout.caption = "\(convo.localParticipantIdentifier) flipped a coin, call it!" // figure out how to get the name
-      
-      let message = MSMessage(session: session)
-      message.layout = layout
-      message.url = nil // deal with URL thing, Apple default?
-      message.summaryText = "Coin flipped."
-      
-      convo.insert(message, completionHandler: { (error: Error?) in
-        print(error)
-      })
-
-    }
+    gameState = nextGameState(from: gameState)
+    
+    let session = convo.selectedMessage?.session ?? MSSession()
+    
+    let layout = MSMessageTemplateLayout()
+    layout.imageTitle = "COIN FLIP CHALLENGE!"
+    layout.image = coinImg.image
+    layout.caption = "Bob flipped a coin, call it!" // figure out how to get the name
+    
+    let message = MSMessage(session: session)
+    message.layout = layout
+    message.url = nil // deal with URL thing, Apple default?
+    message.summaryText = "Coin flipped."
+    
+    convo.insert(message, completionHandler: { (error: Error?) in
+      print(error)
+    })
   }
   
   func nextGameState(from state: GameState?) -> GameState {
