e9f244a7ef5a09a52a43bfa398b2a9707cb85b16
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2016-11-22 09:53:19 -0500

    fade in/out on sound assets, changed icon color

---
+++ MessagesExtension/Art Attribution.swift
@@ -0,0 +1,19 @@
+//
+//  Art Attribution.swift
+//  MatchUp
+//
+//  Created by Drew Lanning on 11/22/16.
+//  Copyright © 2016 Drew Lanning. All rights reserved.
+//
+
+
+// All icons procured from The Noun Project and used under the Creative Commons license, see below.
+// CC Human Readable Link: https://creativecommons.org/licenses/by/3.0/us/
+// CC Full License: https://creativecommons.org/licenses/by/3.0/us/legalcode
+//
+// Die - Dice by Milky - Digital innovation from the Noun Project
+// Single Die "1" through "6" - Black dice by Marta Ambrosetti from the Noun Project
+// Ship - sail ship by Andrejs Kirma from the Noun Project
+// Captain - captain by lastspark from the Noun Project
+// Crew - team by Luis Prado from the Noun Project
+// Jolly Roger - Tattered Jolly Roger Flag by Andrew Cameron from the Noun Project
